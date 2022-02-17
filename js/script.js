af8083c09a2f829e5e5fb2abc5b7d09c55af36d5
Muhammad-Mehedi-Hasan-Opi-Sarker <opisarker99@gmail.com>
2022-02-17 19:57:59 +0600

    first error

--- js/script.js
+++ js/script.js
@@ -3,15 +3,22 @@ document.getElementById('calculate-button').addEventListener('click', function (
     const food = document.getElementById('food');
     const rent = document.getElementById('rent');
     const clothes = document.getElementById('clothes');
+    const now = document.getElementById('now');
     // total expenses 
     const totalExpenses = document.getElementById('total-expenses');
-    foodRentClothe = parseInt(food.value) + parseInt(rent.value) + parseInt(clothes.value);
-    totalExpenses.innerText = foodRentClothe;
+    if (income.value > 0) {
+        foodRentClothe = parseInt(food.value) + parseInt(rent.value) + parseInt(clothes.value);
+        totalExpenses.innerText = foodRentClothe;
 
-    // balance cheek 
-    const balance = document.getElementById('balance');
-    total = parseInt(income.value) - foodRentClothe;
-    balance.innerText = total;
+        // balance cheek 
+        const balance = document.getElementById('balance');
+        total = parseInt(income.value) - foodRentClothe;
+        balance.innerText = total;
+        now.style.display = 'none';
+    }
+    else {
+        now.style.display = 'block';
+    }
 
     // empty file 
     // income.value = '';
@@ -23,18 +30,24 @@ document.getElementById('calculate-button').addEventListener('click', function (
 // saving calculation
 
 document.getElementById('save-button').addEventListener('click', function () {
+
+
+    // hide 
     const saveInput = document.getElementById('save-input');
     const saveAmount = document.getElementById('save-amount');
     const income = document.getElementById('income');
     let saveParcent = parseInt(saveInput.value) / 100;
     let total = parseInt(income.value) * saveParcent;
-    saveAmount.innerText = total;
+    saveAmount.innerText = total.toFixed(2);
     // Remaining Balance
-
     const balance = document.getElementById('balance');
-    let remaining = parseInt(balance.innerText) - total;
+
     const remainingBalance = document.getElementById('remaining-balance');
-    remainingBalance.innerText = remaining;
+    let remaining = parseInt(balance.innerText) - total;
+    remainingBalance.innerText = remaining
+
+
+
 
     // saveAmount.innerText = parseInt(saveInput.value);
 })
